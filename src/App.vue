e83e43d6a668cbb4dc3a9d1cc61d16fd54392070
Tidaporn <24907@watsw.ac.th>
2020-12-03 12:46:07 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <Kanban :data="data"/>
+  </div>
+</template>
+
+<script>
+import Kanban from "./components/Kanban"
+
+
+export default {
+  name: 'App',
+  components: {
+    Kanban
+  },
+  data(){
+    return {
+      data:[
+        {
+          name:"todo",
+          color:"#F5B271",
+          tasks:[]
+        },
+         {
+          name:"doing",
+          color:"#BEA771",
+          tasks:[]
+        },
+         {
+          name:"done",
+          color:"#1B796F",
+          tasks:[]
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  #app {
+  height: 100vh;
+  }
+</style>
