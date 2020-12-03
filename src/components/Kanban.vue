f126a8d65f6244caf16028777c9c7fdb83e6a016
Tidaporn <24907@watsw.ac.th>
2020-12-03 17:46:18 +0700

    nook

--- src/components/Kanban.vue
+++ src/components/Kanban.vue
@@ -22,9 +22,9 @@
           <div class="drop_zone" 
           @dragenter.prevent="drop_zone_enter" 
           @dragleave.prevent="drop_zone_leave"
-          @dragover.prevent
-           >
-
+          @dragover.prevent 
+          @drop="drop_item(index,task_index)"
+          >
           </div>
         </div>
 
@@ -46,6 +46,7 @@ export default {
   props: {
     data: Array,
     create_task_submit: Function,
+    move_item_task:Function
   },
   methods: {
     create_task(index_column) {
@@ -70,6 +71,14 @@ export default {
       event.target.style.height = "10px";
         event.target.style.borderStyle = "none";
         event.target.style.transition = "height 0.5s";
+    },
+    drop_item(column_index,task_index){
+      this.move_item_task(
+        this.current_column_index,
+        this.current_task_index,
+        column_index,
+        task_index
+      )
     }
   },
   data() {
